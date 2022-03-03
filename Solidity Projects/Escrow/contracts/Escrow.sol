c0d88423e1ffc3260c94ab62cbf0745a7e86489b
NotConfident <teeyongteng@gmail.com>
2022-03-03 17:33:22 +0800

    feat: updated object into structs, added mappings for address to structs

--- Solidity Projects/Escrow/contracts/Escrow.sol
+++ Solidity Projects/Escrow/contracts/Escrow.sol
@@ -3,17 +3,33 @@ pragma solidity ^0.8.0;
 
 contract Escrow {
     address public owner;
-    address public partyA;
-    address public partyB;
+
+    struct Party {
+        address userAddress;
+        address token;
+        uint256 amount;
+    }
+
+    // 0 - Party A
+    // 1 - Party B
+    Party[2] public party;
+    mapping(address => Party) public addressToParty;
 
     constructor() public {
-        partyA = msg.sender;
+        party[0].userAddress = msg.sender;
         owner = 0xA04C70cab4129a79936C651107cEE1149fB3B6be;
     }
 
     enum VAULT_STATE {
         OPEN,
         PAUSED,
+        COMPLETED,
+        CANCELLED
+    }
+
+    enum STATUS {
+        DEPOSITED,
+        AWAITING_DEPOSIT,
         CLOSED
     }
 
@@ -27,27 +43,48 @@ contract Escrow {
 
     function startVault(
         address _partyB,
-        address _token,
-        uint256 _amount,
+        address _tokenA,
+        address _tokenB,
+        uint256 _amountA,
+        uint256 _amountB,
         uint256 _time
     ) public {
         vault_state = VAULT_STATE.OPEN;
+        party[0].userAddress = msg.sender;
+        party[0].token = _tokenA;
+        party[0].amount = _amountA;
+
+        party[1].userAddress = _partyB;
+        party[1].token = _tokenB;
+        party[1].amount = _amountB;
+
+        addressToParty[msg.sender] = party[0];
+        addressToParty[_partyB] = party[1];
     }
 
-    function deposit(address _token, uint256 _amount) public payable {
+    function deposit(
+        address _token,
+        uint256 _amountA,
+        uint256 _amountB
+    ) public payable {
         // check for vault status
-        require(msg.sender == partyA || msg.sender == partyB);
+        // require(msg.sender == partyA || msg.sender == partyB, "Unauthorized");
+        // require(tokenAddressToAmount[_token] != 0, "Invalid Token");
+        // require(
+        //     amountA == _amountA || amountB == _amountB,
+        //     "Amount does not match"
+        // );
     }
 
     function withdraw(address _token, uint256 _amount) public payable {
         // check for vault status
-        require(msg.sender == partyA || msg.sender == partyB);
+        // require(msg.sender == partyA || msg.sender == partyB);
     }
 
-    function initiateSwap() internal payable {
+    function initiateSwap() public payable {
         // check for vault status
         // start transferring
-        vault_state = VAULT_STATE.CLOSED;
+        vault_state = VAULT_STATE.COMPLETED;
     }
 
     function pauseVault() private {
