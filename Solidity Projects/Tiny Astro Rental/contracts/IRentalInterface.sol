ebf2313e39e4f8e215d4e6c501d371a5566ac8c8
NotConfident <teeyongteng@gmail.com>
2022-12-05 18:59:14 +0800

    feat: code and gas optimization in creating tenant listings, verify onchain rental during tenant acceptance

---
+++ Solidity Projects/Tiny Astro Rental/contracts/IRentalInterface.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IRentalInterface {
+    function rentalRecipientStatus(
+        address recipient
+        ) external;
+}
--- Solidity Projects/Tiny Astro Rental/contracts/Rental.sol
+++ Solidity Projects/Tiny Astro Rental/contracts/Rental.sol
@@ -7,6 +7,8 @@ contract Rental {
     address public owner;
     IERC20 private token;
     
+    address AstroStakingControllerV3 = 0x1be7aC1d4974C9920E30E2DBC3a57F9e0e1c8EF2;
+
     event Received(address, uint256);
 
     modifier onlyOwner() { // For emergency cases that requires interference
@@ -23,18 +25,17 @@ contract Rental {
     }
 
     struct TenantListing { // To Rent (Non TA Holder)
-        uint256[] tokenAmount; // Qty of the Token to be used
-        uint16[] rentDuration;
+        uint256 tokenAmount; // Qty of the Token to be used
+        uint16 rentDuration;
         address userAddress;
         address delegatedWallet;
         LISTING_INFO status;
     }
 
     struct LandLordListing { // To Rent Out (TA Holder)
-        uint256[] tokenAmount; // Qty of the Token to be used / slot
-        uint16[] rentDuration; // Duration of the slot to be rented out
+        uint256 tokenAmount; // Qty of the Token to be used / slot
+        uint16 rentDuration; // Duration of the slot to be rented out
         address userAddress;
-        uint256 numSlots; // Number of slots to rent out
         LISTING_INFO status;
     }
 
@@ -42,53 +43,60 @@ contract Rental {
     mapping(uint256 => TenantListing) public tenantListingMap;
     mapping(uint256 => bool) landlordListingsActive;
     mapping(uint256 => bool) tenantListingsActive;
-
+    
     uint256 public landLordListingID = 0;
     uint256 public tenantListingID = 0;
 
     // WRITE
     // Create Listings - Tenant, Want to Rent (Non TA Holder)
-    function createListingTenant(uint256[] calldata tokenQty, uint16[] calldata slotDuration, address delegateWallet) public {
-        TenantListing memory listing;
+    function createListingTenant(uint256 ethQty, uint16 slotDuration, address delegateWallet) public payable {
+        TenantListing storage listing = tenantListingMap[tenantListingID];
 
-        listing.tokenAmount = tokenQty;
+        listing.tokenAmount = ethQty;
         listing.rentDuration = slotDuration;
         listing.userAddress = msg.sender;
-        listing.delegatedWallet = delegateWallet != address(0) ? delegateWallet : address(0);
-        listing.status = LISTING_INFO.OPEN;
+        listing.delegatedWallet = delegateWallet;
+        // listing.status = LISTING_INFO.OPEN; // Default open upon creation
 
-        tenantListingMap[tenantListingID] = listing;
-        tenantListingsActive[tenantListingID] = true;
+        // tenantListingMap[tenantListingID] = listing; // Mapped on Line 53
+        // tenantListingsActive[tenantListingID] = true;
+
         tenantListingID += 1;
+        require(msg.sender == tx.origin);
+        require(msg.value == ethQty, "Not enough ETH");
     }
 
     // Create Listings - Landlord, Want to Rent Out (TA Holder)
     // 1000000000000000000 - 1 ETH
-    function createListingLandlord(uint256[] calldata tokenQty, uint16[] calldata slotDuration, uint256 numSlots) public {
-        LandLordListing memory listing;
+    function createListingLandlord(uint256 ethQty, uint16 slotDuration) public {
+        LandLordListing storage listing = landlordListingsMap[landLordListingID];
 
-        listing.tokenAmount = tokenQty;
+        listing.tokenAmount = ethQty;
         listing.rentDuration = slotDuration;
         listing.userAddress = msg.sender;
-        listing.numSlots = numSlots;
-        listing.status = LISTING_INFO.OPEN;
+        // listing.status = LISTING_INFO.OPEN; // Default open upon creation
 
-        landlordListingsMap[landLordListingID] = listing;
-        landlordListingsActive[landLordListingID] = true;
+        // landlordListingsMap[landLordListingID] = listing; // Mapped on Line 71
+        // landlordListingsActive[landLordListingID] = true;
         landLordListingID += 1;
     }
 
 
-    // Accept Listings - Renting (Non TA Holder)
+    // Accept Listings - Tenant (Non TA Holder)
 
 
-
-    // Accept Listings - Renter (TA Holder)
+    // Accept Listings - Landlord (TA Holder) // Use rentalRecipientStatus() to verify
     function acceptListing(uint256 id) public {
-        
+        TenantListing memory listing;
+        listing = tenantListingMap[id];
+
+        bool isValid;
+        address tenantWallet = listing.delegatedWallet;
+
+        // Verify if Landlord has Rented out pass to Tenant on chain
+        (isValid, ) = AstroStakingControllerV3.call(abi.encodeWithSignature("rentalRecipientStatus(address)", tenantWallet));
+        require(isValid == true);
     }
-
-
 
     // READ
     function viewLandLordListings(uint256 id) public view returns(LandLordListing memory listing) {
@@ -109,7 +117,6 @@ contract Rental {
         token.transfer(owner, IERC20(_token).balanceOf(address(this)));
     }
 
-
     receive() external payable {
         emit Received(msg.sender, msg.value);
     }
@@ -117,10 +124,8 @@ contract Rental {
     fallback() external payable{}
 }
 
-// interface RentalInterface {
-//     function transferFrom(
-//         address from,
-//         address to,
-//         uint256 tokenId
-//     ) external;
-// }
+interface IRentalInterface {
+    function rentalRecipientStatus(
+        address recipient
+        ) external;
+}
